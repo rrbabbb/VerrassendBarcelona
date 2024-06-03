e004557e81c1d15fbc6ff5c114fbc672194d950d
david <davidnagler24@gmail.com>
2024-06-03 14:57:10 +0200

    database class toegevoegd

---
+++ ClassLibrary1/Database.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ClassLibrary1;
+
+public class Database : IDisposable
+{
+    private string connectionString = new MySqlConnectionStringBuilder
+    {
+        UserID = "root",
+        Password = "root",
+        Port = 3306,
+        Server = "localhost",
+        Database = " "
+    }.ToString();
+    private MySqlConnection connection;
+
+    public Database()
+    {
+        connection = new(connectionString);
+        connection.Open();
+    }
+
+    public void Dispose()
+    {
+        connection.Close();
+        GC.SuppressFinalize(this);
+    }
+}
